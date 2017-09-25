e0ab2397c2520dba7f79c3d7f7235f7cff9fb838
Cloris <cloristang97@gmail.com>
2017-09-24 21:07:06 -0400

    new page, simple nav bar

---
+++ main.js
@@ -0,0 +1,64 @@
+function roll() {
+	// return a number from 1 to 12.
+	return Math.floor(Math.random()*12) + 1;
+}
+var step = parseInt(sessionStorage.getItem("rolled"));
+console.log('showing section '+step);
+function switchShow(n) {
+	switch (n) {
+		case 1:
+		$('.thank').removeClass('hidden').addClass('animated fadeIn');
+		$('section').not('.thank, .hidden').addClass('hidden').removeClass('animated fadeIn');
+		break;
+		case 2:
+		$('.experience').removeClass('hidden').addClass('animated fadeIn');
+		$('section').not('.experience, .hidden').addClass('hidden').removeClass('animated fadeIn');
+		break;
+		case 3:
+		$('.give').removeClass('hidden').addClass('animated fadeIn');
+		$('section').not('.give, .hidden').addClass('hidden').removeClass('animated fadeIn');
+		break;
+		case 4:
+		$('.hike').removeClass('hidden').addClass('animated fadeIn');
+		$('section').not('.hike, .hidden').addClass('hidden').removeClass('animated fadeIn');
+		break;
+		case 5:
+		$('.help').removeClass('hidden').addClass('animated fadeIn');
+		$('section').not('.help, .hidden').addClass('hidden').removeClass('animated fadeIn');
+		break;
+		case 6:
+		$('.meditate').removeClass('hidden').addClass('animated fadeIn');
+		$('section').not('.meditate, .hidden').addClass('hidden').removeClass('animated fadeIn');
+		break;
+		case 7:
+		$('.eat').removeClass('hidden').addClass('animated fadeIn');
+		$('section').not('.eat, .hidden').addClass('hidden').removeClass('animated fadeIn');
+		break;
+		case 8:
+		$('.socialize').removeClass('hidden').addClass('animated fadeIn');
+		$('section').not('.socialize, .hidden').addClass('hidden').removeClass('animated fadeIn');
+		break;
+		case 9:
+		$('.exercise').removeClass('hidden').addClass('animated fadeIn');
+		$('section').not('.exercise, .hidden').addClass('hidden').removeClass('animated fadeIn');
+		break;
+		case 10:
+		$('.aim').removeClass('hidden').addClass('animated fadeIn');
+		$('section').not('.aim, .hidden').addClass('hidden').removeClass('animated fadeIn');
+		break;
+		case 11:
+		$('.rest').removeClass('hidden').addClass('animated fadeIn');
+		$('section').not('.rest, .hidden').addClass('hidden').removeClass('animated fadeIn');
+		break;
+		default:
+		$('.smile').removeClass('hidden').addClass('animated fadeIn');
+		$('section').not('.smile, .hidden').addClass('hidden').removeClass('animated fadeIn');
+	}
+}
+switchShow(step);
+$('#shuffle').on('click', function() {
+	step = roll();
+	sessionStorage.setItem("rolled", step);
+	switchShow(step);
+	// console.log('showing '+step);
+})
